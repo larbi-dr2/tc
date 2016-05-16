0ff956950b94ba6a44a2dd28f04a166173ad6875
larbi-dr2 <larbi@dr-2.eu>
2016-05-16 13:02:36 +0000

    first local commit

---
+++ timechecker0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="timechecker0.MvcApplication" Language="C#" %>
---
+++ timechecker0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(timechecker0.Startup))]
+namespace timechecker0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
